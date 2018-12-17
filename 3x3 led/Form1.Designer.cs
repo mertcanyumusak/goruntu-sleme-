f210e09a026eeca37bb23e5db7da8b3c5b038596
mertcanyumusak <45950124+mertcanyumusak@users.noreply.github.com>
2018-12-18 02:09:28 +0300

    projeler
    visual studio ve arduino kodları 1.proje için

---
+++ 3x3 led/Form1.Designer.cs
@@ -0,0 +1,381 @@
+﻿namespace _3x3_led
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rdiobtnKirmizi = new System.Windows.Forms.RadioButton();
+            this.rdbtnElleBelirleme = new System.Windows.Forms.RadioButton();
+            this.rdiobtnYesil = new System.Windows.Forms.RadioButton();
+            this.rdiobtnMavi = new System.Windows.Forms.RadioButton();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.bagkes = new System.Windows.Forms.Button();
+            this.baglan = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tool = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rdiobtnKirmizi
+            // 
+            this.rdiobtnKirmizi.AutoSize = true;
+            this.rdiobtnKirmizi.Location = new System.Drawing.Point(391, 335);
+            this.rdiobtnKirmizi.Name = "rdiobtnKirmizi";
+            this.rdiobtnKirmizi.Size = new System.Drawing.Size(68, 17);
+            this.rdiobtnKirmizi.TabIndex = 73;
+            this.rdiobtnKirmizi.TabStop = true;
+            this.rdiobtnKirmizi.Text = "Kırmızı(R)";
+            this.rdiobtnKirmizi.UseVisualStyleBackColor = true;
+            // 
+            // rdbtnElleBelirleme
+            // 
+            this.rdbtnElleBelirleme.AutoSize = true;
+            this.rdbtnElleBelirleme.Location = new System.Drawing.Point(391, 404);
+            this.rdbtnElleBelirleme.Name = "rdbtnElleBelirleme";
+            this.rdbtnElleBelirleme.Size = new System.Drawing.Size(87, 17);
+            this.rdbtnElleBelirleme.TabIndex = 77;
+            this.rdbtnElleBelirleme.TabStop = true;
+            this.rdbtnElleBelirleme.Text = "Elle Belirleme";
+            this.rdbtnElleBelirleme.UseVisualStyleBackColor = true;
+            // 
+            // rdiobtnYesil
+            // 
+            this.rdiobtnYesil.AutoSize = true;
+            this.rdiobtnYesil.Location = new System.Drawing.Point(391, 358);
+            this.rdiobtnYesil.Name = "rdiobtnYesil";
+            this.rdiobtnYesil.Size = new System.Drawing.Size(64, 17);
+            this.rdiobtnYesil.TabIndex = 76;
+            this.rdiobtnYesil.TabStop = true;
+            this.rdiobtnYesil.Text = "Yeşil (G)";
+            this.rdiobtnYesil.UseVisualStyleBackColor = true;
+            // 
+            // rdiobtnMavi
+            // 
+            this.rdiobtnMavi.AutoSize = true;
+            this.rdiobtnMavi.Location = new System.Drawing.Point(391, 381);
+            this.rdiobtnMavi.Name = "rdiobtnMavi";
+            this.rdiobtnMavi.Size = new System.Drawing.Size(64, 17);
+            this.rdiobtnMavi.TabIndex = 74;
+            this.rdiobtnMavi.TabStop = true;
+            this.rdiobtnMavi.Text = "Mavi (B)";
+            this.rdiobtnMavi.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(640, 325);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(25, 13);
+            this.label8.TabIndex = 72;
+            this.label8.Text = "255";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(605, 325);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(25, 13);
+            this.label7.TabIndex = 71;
+            this.label7.Text = "255";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(572, 325);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 13);
+            this.label6.TabIndex = 70;
+            this.label6.Text = "255";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(644, 311);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 13);
+            this.label5.TabIndex = 69;
+            this.label5.Text = "B";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(608, 311);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 13);
+            this.label4.TabIndex = 68;
+            this.label4.Text = "G";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(575, 311);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 13);
+            this.label3.TabIndex = 67;
+            this.label3.Text = "R";
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(634, 335);
+            this.trackBar3.Maximum = 255;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar3.Size = new System.Drawing.Size(45, 123);
+            this.trackBar3.TabIndex = 66;
+            this.trackBar3.Value = 1;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(601, 335);
+            this.trackBar2.Maximum = 255;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar2.Size = new System.Drawing.Size(45, 123);
+            this.trackBar2.TabIndex = 65;
+            this.trackBar2.Value = 1;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(568, 335);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 123);
+            this.trackBar1.TabIndex = 64;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "COM3",
+            "COM4"});
+            this.comboBox2.Location = new System.Drawing.Point(180, 383);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(75, 21);
+            this.comboBox2.TabIndex = 63;
+            // 
+            // bagkes
+            // 
+            this.bagkes.Location = new System.Drawing.Point(180, 352);
+            this.bagkes.Name = "bagkes";
+            this.bagkes.Size = new System.Drawing.Size(75, 23);
+            this.bagkes.TabIndex = 62;
+            this.bagkes.Text = "Bağ Kes";
+            this.bagkes.UseVisualStyleBackColor = true;
+            this.bagkes.Click += new System.EventHandler(this.bagkes_Click);
+            // 
+            // baglan
+            // 
+            this.baglan.Location = new System.Drawing.Point(180, 320);
+            this.baglan.Name = "baglan";
+            this.baglan.Size = new System.Drawing.Size(75, 23);
+            this.baglan.TabIndex = 61;
+            this.baglan.Text = "Bağlan";
+            this.baglan.UseVisualStyleBackColor = true;
+            this.baglan.Click += new System.EventHandler(this.baglan_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(5, 410);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(144, 21);
+            this.comboBox1.TabIndex = 60;
+            // 
+            // button3
+            // 
+            this.button3.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button3.Location = new System.Drawing.Point(5, 378);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(144, 26);
+            this.button3.TabIndex = 59;
+            this.button3.Text = "Çıkış";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(5, 318);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 26);
+            this.button1.TabIndex = 57;
+            this.button1.Text = "Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(5, 350);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 26);
+            this.button2.TabIndex = 58;
+            this.button2.Text = "Durdur";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(370, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(330, 300);
+            this.pictureBox2.TabIndex = 56;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(330, 300);
+            this.pictureBox1.TabIndex = 55;
+            this.pictureBox1.TabStop = false;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tool,
+            this.toolStripLabel1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 496);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(732, 25);
+            this.toolStrip1.TabIndex = 78;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tool
+            // 
+            this.tool.Name = "tool";
+            this.tool.Size = new System.Drawing.Size(0, 22);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(86, 22);
+            this.toolStripLabel1.Text = "toolStripLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 521);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.rdiobtnKirmizi);
+            this.Controls.Add(this.rdbtnElleBelirleme);
+            this.Controls.Add(this.rdiobtnYesil);
+            this.Controls.Add(this.rdiobtnMavi);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.bagkes);
+            this.Controls.Add(this.baglan);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rdiobtnKirmizi;
+        private System.Windows.Forms.RadioButton rdbtnElleBelirleme;
+        private System.Windows.Forms.RadioButton rdiobtnYesil;
+        private System.Windows.Forms.RadioButton rdiobtnMavi;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button bagkes;
+        private System.Windows.Forms.Button baglan;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel tool;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+    }
+}
+
